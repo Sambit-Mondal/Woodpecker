e2dc5097604965946a04bd813d7486d33fd920bf
Sambit Mondal <sambitmondal2005@gmail.com>
2024-07-18 15:21:16 +0530

    Update style

--- client/src/components/LeftBox.jsx
+++ client/src/components/LeftBox.jsx
@@ -27,20 +27,20 @@ function LeftBox() {
     }
 
   return (
-    <div className='w-auto lg:w-[95vw] h-full lg:h-[70%] m-5 lg:m-5 py-5 lg:py-0 px-5 flex flex-col items-center justify-center gap-5 bg-bg rounded-sm'>
+    <div className='w-full lg:w-[95vw] h-full lg:h-[70%] m-5 lg:mr-5 lg:my-5 lg:ml-0 py-5 lg:py-0 px-5 flex flex-col items-center justify-center gap-5 bg-bg rounded-sm'>
         <div className='flex flex-wrap flex-[0.5] bg-screen lg:w-full h-full lg:h-full rounded-sm mb-4'>
             {earthquake && <Earthquake />}
             {flood && <Flood />}
             {cyclone && <Cyclone />}
         </div>
         <div>
-            <button className='bg-white text-black p-3 px-8 flex items-center justify-center font-semibold rounded-[0.25rem]'>Show data</button>
+            <button className='bg-white text-black p-3 px-8 flex items-center justify-center font-semibold rounded-md'>Show data</button>
         </div>
         <hr className='w-full border-0 bg-white h-[2px] mb-2 mt-5 lg:mt-5' />
         <div className='flex items-center justify-around w-full gap-1 gap-y-3 xl:gap-3 flex-wrap'>
-            <button className='bg-white border-[1px] border-black text-black text-sm p-3 flex items-center justify-center font-semibold rounded-[0.25rem] transition duration-150 ease-in-out hover:bg-gray-500 hover:text-white' onClick={isEarthquake}>Earthquake</button>
-            <button className='bg-white border-[1px] border-black text-black text-sm p-3 px-7 flex items-center justify-center font-semibold rounded-[0.25rem] transition duration-150 ease-in-out hover:bg-gray-500 hover:text-white' onClick={isFlood}>Flood</button>
-            <button className='bg-white border-[1px] border-black text-black text-sm p-3 px-7 flex items-center justify-center font-semibold rounded-[0.25rem] transition duration-150 ease-in-out hover:bg-gray-500 hover:text-white' onClick={isCyclone}>Cyclone</button>
+            <button className='bg-white border-[1px] border-black text-black text-sm p-3 flex items-center justify-center font-semibold rounded-full transition duration-150 ease-in-out hover:bg-gray-500 hover:text-white' onClick={isEarthquake}>Earthquake</button>
+            <button className='bg-white border-[1px] border-black text-black text-sm p-3 px-7 flex items-center justify-center font-semibold rounded-full transition duration-150 ease-in-out hover:bg-gray-500 hover:text-white' onClick={isFlood}>Flood</button>
+            <button className='bg-white border-[1px] border-black text-black text-sm p-3 px-7 flex items-center justify-center font-semibold rounded-full transition duration-150 ease-in-out hover:bg-gray-500 hover:text-white' onClick={isCyclone}>Cyclone</button>
         </div>
     </div>
   )
--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -6,7 +6,7 @@ import Chatbot from '../components/Chatbot'
 
 function Home() {
   return (
-    <div className="bg-url bg-no-repeat bg-cover bg-center select-none w-full h-auto overflow-auto lg:h-full flex flex-col items-center justify-center text-white m-0 p-0 box-border">
+    <div className="bg-url bg-no-repeat bg-cover bg-center select-none w-screen h-auto overflow-auto lg:h-full flex flex-col items-center justify-center text-white m-0 p-0 box-border">
       <div className='w-screen h-full flex justify-center'>
         <Navbar />
       </div >
